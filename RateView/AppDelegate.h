329508efdc928d3daf70bfa2ae017d389db2a83f
cheyuxin <cheyuxin@talicai.com>
2015-07-30 10:05:43 +0800

    Initial Commit

---
+++ RateView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RateView
+//
+//  Created by Che Yuxin on 15/7/30.
+//  Copyright (c) 2015年 Che Yuxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RateView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RateView
+//
+//  Created by Che Yuxin on 15/7/30.
+//  Copyright (c) 2015年 Che Yuxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
